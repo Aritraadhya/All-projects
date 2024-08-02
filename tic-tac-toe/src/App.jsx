78ded4e643191d106d838bf08d483cf3ef14e6c7
Aritraadhya <aritraadhya1@gmail.com>
2024-08-02 14:14:55 +0530

    files added

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Grid from './components/Grid/Grid'
+import Card from './components/Card/Card'
+function App() {
+  return (
+    <>
+    <Grid numberOfCards={9} />
+    </>
+  )
+}
+
+export default App
